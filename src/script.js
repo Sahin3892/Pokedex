8644df916e9327b3ab7ab832d50891b2c7edb7ff
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2023-09-17 16:36:01 +0200

    with details about Pokemon

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ let currentPokemon = []; // Die geladenen Pokémon zu speichern
 let offset = 0; // Der Startwert für den Offset zum Laden der Pokémon
 const limit = 40; // Die Anzahl der pro Ladung angeforderten Pokémon
 let currentIndex = 0;
+
 // Funktion zum Laden der ersten 40 Pokémon
 async function loadPokemon() {
   let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
@@ -21,7 +22,7 @@ async function renderPokemonList() {
     let pokemon = currentPokemon[i];
     let url = pokemon.url;
 
-    // Lade detaillierte Daten für jedes Pokémon
+    // Lade detaillierte Daten für jedes Pokémon:
     let response = await fetch(url);
     let detailedData = await response.json();
 
@@ -34,17 +35,20 @@ async function renderPokemonList() {
     // Extrahiere den Typ des Pokemons aus dem Zweiten Typ
     let type2Text = types.length > 1 ? types[1].type.name : "";
 
-
     // Finde die passende Hintergrundfarbe basierend auf dem Typ
     let backgroundColor = getBackgroundColorByType(typeText);
     console.log(detailedData);
     // Füge Name, Typ und Bild jedes Pokémon zur Anzeige hinzu
     renderContainer.innerHTML += /*html*/ `
       <div onclick="openCard(${i})" class="cards" style="background-color: ${backgroundColor}">
-        <p>${detailedData.name}</p>
-        <p>${typeText}</p>
-        <p>${type2Text}</p>
-        <img src="${detailedData.sprites.front_default}" alt="${detailedData.name}">
+        <div class="card-content">
+          <div class="card-text">
+            <h3 class="cardName">${detailedData.name}</h3>
+            <p class="cardTyp">${typeText}</p>
+            <p class="cardTyp">${type2Text}</p>
+          </div>
+          <img class="cardIMG" src="${detailedData.sprites.front_default}" alt="${detailedData.name}">
+        </div>
       </div>
     `;
   }
@@ -68,6 +72,7 @@ async function loadMorePokemon() {
 
 // Funktion zum Öffnen der Karte für ein ausgewähltes Pokémon
 function openCard(i) {
+  currentIndex = i; // Aktualisiere den globalen Index
   let pokemon = currentPokemon[i];
   let url = pokemon.url;
 
@@ -76,17 +81,36 @@ function openCard(i) {
     .then((detailedData) => {
       let types = detailedData.types;
       let typeText = types.length > 0 ? types[0].type.name : "Unknown";
-
       let backgroundColor = getBackgroundColorByType(typeText);
 
       let overlay = document.getElementById("overlay");
       overlay.style.display = "block"; // Zeige das Overlay an
 
+      // Verarbeite die Abilities im Array mit einer Schleife
+      let abilitiesText = "";
+      for (let ability of detailedData.abilities) {
+        abilitiesText += ability.ability.name + ", ";
+      }
+      // Entferne das letzte Komma und Leerzeichen
+      abilitiesText = abilitiesText.slice(0, -2);
+
       let overlayContent = /*html*/ `
+        <div class="leftRight">
+          <img onclick="moveLeft()" src="src/img/left.png" />
+          <img onclick="moveRight()" src="src/img/right.png" />
+        </div>
         <div class="overlay-card" style="background-color: ${backgroundColor}">
-          <p>${detailedData.name}</p>
-          <p>${typeText}</p>
-          <img src="${detailedData.sprites.front_default}" alt="${detailedData.name}">
+          <div>
+            <h4>${detailedData.name}</h4>
+            <p>${typeText}</p>
+            <img class="imgOverlay" src="${detailedData.sprites.front_default}" alt="${detailedData.name}">
+          </div>
+          <div class="pokeDetails">
+            <p>Species: ${detailedData.species.name}</p>
+            <p>Height: ${detailedData.height}</p>
+            <p>Weight: ${detailedData.weight}</p>
+            <p>Abilities: ${abilitiesText}</p>
+          </div>
         </div>
       `;
 
@@ -118,6 +142,32 @@ function closeCardOutside(event) {
   if (!overlayCard.contains(event.target)) {
     closeCard(); // Schließe die Karte
   }
+}
+
+function moveRight() {
+  // Überprüfe, ob currentIndex am Ende der Liste ist
+  if (currentIndex === currentPokemon.length - 1) {
+    // Wenn ja, gehe zum Anfang der Liste
+    currentIndex = 0;
+  } else {
+    // Andernfalls gehe zum nächsten Pokémon
+    currentIndex++;
+  }
+  // Rufe openCard() direkt hier auf
+  openCard(currentIndex);
+}
+
+function moveLeft() {
+  // Überprüfe, ob currentIndex am Anfang der Liste ist
+  if (currentIndex === 0) {
+    // Wenn ja, gehe zum Ende der Liste
+    currentIndex = currentPokemon.length - 1;
+  } else {
+    // Andernfalls gehe zum vorherigen Pokémon
+    currentIndex--;
+  }
+  // Rufe openCard() direkt hier auf
+  openCard(currentIndex);
 }
 
 // Funktion, um die Hintergrundfarbe basierend auf dem Typ zu erhalten
